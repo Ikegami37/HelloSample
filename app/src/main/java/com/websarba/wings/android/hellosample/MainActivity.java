3357630c8ad9d2d0db3e594d3cf128fd483e1069
Ikegami37 <92580968+Ikegami37@users.noreply.github.com>
2022-02-26 13:52:57 +0900

    リスナの設定

--- app/src/main/java/com/websarba/wings/android/hellosample/MainActivity.java
+++ app/src/main/java/com/websarba/wings/android/hellosample/MainActivity.java
@@ -3,6 +3,10 @@ package com.websarba.wings.android.hellosample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btClick = findViewById(R.id.btClick);
+        HelloListener listener = new HelloListener();
+        btClick.setOnClickListener(listener);
+    }
+    private class HelloListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+            String inputStr = input.getText().toString();
+            output.setText(inputStr + "さん、こんにちわ！");
+        }
     }
 }
